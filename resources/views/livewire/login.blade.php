9e5b8809395273e3fc30fd0de8019c25f07902e9
Felipe Castro <tecnologia.fulltime@gmail.com>
2024-09-02 18:10:18 -0300

    Melhoria no layout do login

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -4,45 +4,45 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Volt\Component;
- 
+
 new
 #[Layout('components.layouts.empty')]
 #[Title('Login')]
 
 class extends Component {
- 
+
     #[Rule('required|email')]
     public string $email = '';
- 
+
     #[Rule('required')]
     public string $password = '';
- 
+
     public function mount()
     {
         if (auth()->user()) {
             return redirect('/');
         }
     }
- 
+
     public function login()
     {
         $credentials = $this->validate();
- 
+
         if (auth()->attempt($credentials)) {
             request()->session()->regenerate();
             return redirect()->intended('/');
         }
- 
+
         $this->addError('email', __('login.credentials_error'));
         $this->addError('password', __('login.credentials_error'));
     }
 } ?>
 
 <div class="grid gap-8 md:grid-cols-3">
-    <div class="text-center order-last md:order-first">
-        <img src="images/timeline.svg" width="400" class="mx-auto" alt="Timeline" />
-    </div>
-    <div>
+    <div class="col-start-2 col-end-2">
+        <div class="text-center order-last md:order-first">
+            <img src="images/timeline.svg" width="250" class="mx-auto" alt="Timeline" />
+        </div>
         <x-card title="{{ __('menu.home')}}" class="px-8" shadow separator>
             <div class="mb-8">
                 Faça login para acessar o sistema e registrar seu ponto.
@@ -56,13 +56,13 @@ class extends Component {
                 </x-slot:actions>
             </x-form>
         </x-card>
-    </div>
-    <div class="mb-5">
-        <x-card shadow>
-            <x-icon name="o-lifebuoy" class="w-10 h-10" />
-            <div class="my-5">
-                Contato do suporte: tecnologia.fulltime@gmail.com
-            </div>
-        </x-card>
+        <div class="mt-5">
+            <x-card shadow>
+                <x-icon name="o-lifebuoy" class="w-10 h-10" />
+                <div class="my-5">
+                    Contato do suporte: tecnologia.fulltime@gmail.com
+                </div>
+            </x-card>
+        </div>
     </div>
 </div>
