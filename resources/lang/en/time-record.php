08033cfc5f8ce1c3130d1943d6dd46147ce40bc4
Felipe Castro <tecnologia.fulltime@gmail.com>
2024-09-03 00:51:23 -0300

    Melhorias

---
+++ resources/lang/en/time-record.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title' => 'Time Record',
+    'create' => 'Create Time Record',
+    'start_date' => 'Start Date',
+    'end_date' => 'End Date',
+    'password' => 'Password',
+    'success' => 'Time Record saved successfully.',
+    'success_edit' => 'Time Record edited successfully.',
+    'employee' => 'Employee',
+    'date' => 'Date',
+    'start' => 'Start',
+    'end' => 'End', 
+];
